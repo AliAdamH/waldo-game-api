93c78b9778cae96b57146b7d3398a5bdbbf09e98
Ali Hassaine <ali.hs@outlook.fr>
2022-10-01 21:59:43 +0000

    Add serialized responses

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::ImagesController < ApplicationController
   
   def index
     @images = Image.all
-    render json: ImageSerializer.new(@images, fields: { image: %i[id title thumbnail_url] }).serializable_hash
+    render json: ImageSerializer.new(@images, fields: { image: %i[id title thumbnail_url] }).serializable_hash[:data]
   end
 
   def show
     @image = Image.find(params[:id])
-    render json: ImageSerializer.new(@image, fields: { image: %i[landscape_url] }).serializable_hash
+    render json: ImageSerializer.new(@image, fields: { image: %i[landscape_url] }).serializable_hash[:data][:attributes]
   end
 
   def verify_coordinates; end

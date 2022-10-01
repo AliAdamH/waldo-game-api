40c67d262fc84134224847bb332eb76f14e57cf5
Ali Hassaine <ali.hs@outlook.fr>
2022-10-01 03:37:00 +0000

    Setup image controller

---
+++ app/controllers/api/v1/image_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ImagesController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def verify_coordinates; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :images, only: %i[index show]
+      get 'verify', to: 'images#verify_coordinates'
+    end
+  end
 end

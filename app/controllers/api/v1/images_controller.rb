535f079dfba7d0c9e35d703c5d82690b037c2355
Ali Hassaine <ali.hs@outlook.fr>
2022-10-02 03:24:45 +0000

    Add coordinate validation

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -10,5 +10,28 @@ class Api::V1::ImagesController < ApplicationController
     render json: ImageSerializer.new(@image, fields: { image: %i[landscape_url] }).serializable_hash[:data][:attributes]
   end
 
-  def verify_coordinates; end
+  def verify_coordinates
+    image = Image.find(params[:id])
+    answer = character_found(image)
+    render json: answer.to_json
+  end
+
+
+
+  private
+
+  def character_found(image)
+    return { found: 'waldo'  } if compare_coordinates(image.waldo_coords)
+    return { found: 'wizard' } if compare_coordinates(image.wizard_coords)
+    return { found: 'wilma'  } if compare_coordinates(image.wilma_coords)
+    return { found: 'odlaw'  } if compare_coordinates(image.odlaw_coords)
+    { found: '' }
+  end
+
+  def compare_coordinates(image_coordinates)
+    width  = params[:width].to_f
+    height = params[:height].to_f
+    width.between?(image_coordinates[:lower_width], image_coordinates[:upper_width]) &&
+    height.between?(image_coordinates[:lower_height], image_coordinates[:upper_height])
+  end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -9,4 +9,17 @@ class Image < ApplicationRecord
   def thumbnail_url
     Rails.application.routes.url_helpers.url_for(thumbnail) if thumbnail.attached?
   end
+
+# Meta-programming: easier than writing four methods manually
+# where the only variation is the character name.
+ %i[waldo wizard wilma odlaw].each do |character|
+  define_method("#{character}_coords") do
+    {
+      upper_width: self.attributes["#{character}_width_upper"],
+      lower_width: self.attributes["#{character}_width_lower"],
+      upper_height: self.attributes["#{character}_height_upper"],
+      lower_height: self.attributes["#{character}_height_lower"]
+    }
+  end
+ end
 end
